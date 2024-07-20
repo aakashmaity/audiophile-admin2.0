4ffa875a3a6c3153933f72c8d15ce65863987d07
AKASH MAITY <akash.maity.csbs25@heritageit.edu.in>
2024-07-20 17:26:40 +0530

    update config setting to deploy

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -3,22 +3,15 @@ import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import fs from 'fs'
 import mime from 'mime-types'
 import { mongooseConnect } from '@/lib/mongoose';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-// export const dynamic = 'auto'
-// export const dynamicParams = true
-// export const revalidate = false
-// export const fetchCache = 'auto'
-// export const runtime = 'nodejs'
-// export const preferredRegion = 'auto'
-// export const maxDuration = 5
 
 
 export async function POST(req) {
     try {
         await mongooseConnect();
 
-        console.log("mongo connected");
+        // console.log("mongo connected");
         const form = new multiparty.Form();
         const {fields,files} = await new Promise ((resolve,reject) => {
             form.parse(req,(err, fields, files) => {
@@ -30,7 +23,7 @@ export async function POST(req) {
             });
         });
         // console.log(form);
-        console.log("file upload");
+        // console.log("file upload");
 
         // create connection with S3
         const client = new S3Client({
@@ -41,7 +34,7 @@ export async function POST(req) {
             },
         });
 
-        console.log("created s3 bucket")
+        // console.log("created s3 bucket")
         // Add all images to S3 bucket named(audiophile-ecommerce)
         const links=[];
         for( const file of files.file)
@@ -60,7 +53,7 @@ export async function POST(req) {
             }));
             const link = `https://${process.env.BUCKET_NAME}.s3.amazonaws.com/${newFilename}`
             links.push(link)
-            console.log(`published image : ${link}`);
+            // console.log(`published image : ${link}`);
         }
         return NextResponse.json({links, success: true, message : "Uploaded file successfully"});
     } catch (error) {
@@ -69,7 +62,3 @@ export async function POST(req) {
     }
     
 }   
-
-// export const config = {
-//     api: {bodyParser:false}
-// }
