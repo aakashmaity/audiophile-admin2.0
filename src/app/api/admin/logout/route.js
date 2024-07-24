cf6dba9811f0503569842f982d32574fcf36f807
AKASH MAITY <akash.maity.csbs25@heritageit.edu.in>
2024-07-24 12:12:27 +0530

    debug logout route

--- src/app/api/admin/logout/route.js
+++ src/app/api/admin/logout/route.js
@@ -2,11 +2,11 @@ import { NextResponse } from "next/server";
 
 export async function GET(req){
     try {
-        const response = NextResponse.json({success: true, message : "Logout successful!"},{status: 200});
+        let response = NextResponse.json({success: true, message : "Logout successful!"},{status: 200});
         
         // remove token from browser cookies
-        response.cookies.delete('token');
-        return response;
+        let success = response.cookies.set('token',"");
+        return success;
     } catch (error) {
         return NextResponse.json({success: false, message: "Logout failed!"},{status:500});
     }
