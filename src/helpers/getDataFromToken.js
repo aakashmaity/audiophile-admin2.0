0a94bc02336847077d789ece4030bf8d73e472ab
AKASH MAITY <akash.maity.csbs25@heritageit.edu.in>
2024-07-24 13:03:48 +0530

    debug logout route

--- src/helpers/getDataFromToken.js
+++ src/helpers/getDataFromToken.js
@@ -3,7 +3,8 @@ import jwt from "jsonwebtoken";
 export async function getDataFromToken(req){
     try {
         const token = req.cookies.get('token')?.value || "" ;
-        const decodeToken = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(token)
+        const decodeToken = await jwt.verify(token, process.env.JWT_SECRET);
         return decodeToken?.id;   // 'id' contains admin._id
     } catch (error) {
         throw new Error(error.message);
